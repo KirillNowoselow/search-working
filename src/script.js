70078d4604fd75d0dba50f1afccb89b48eceb504
xa4ipur4ik <kirillnowoselow4@gmail.com>
2023-03-23 19:31:15 +0300

    modal reg

---
+++ src/script.js
@@ -0,0 +1,9 @@
+document.querySelector('.reg').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.registration').classList.toggle('open');
+})
+
+document.querySelector('.btn-registration').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.registration').classList.toggle('open');
+})
